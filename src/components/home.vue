754a06b6d7260e7a68cb20f2dd8442c3a304c2fd
zhuoyi <776870907@qq.com>
2019-02-16 11:11:13 +0800

    添加用户数据

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,99 +1,105 @@
 <template>
-    <div id="main">
-        <el-container>
-            <el-header class="header">
-                <el-row>
-                <el-col :span="4">
-                    <img src="@/assets/logo.png">
-                </el-col>
-                <el-col :span="19" class="middle">
-                    <h2>电商后台管理系统</h2>
-                </el-col>
-                <el-col :span="1">
-                    <a class="layout" @click.prevent='homeExit()' href="#">退出</a>
-                </el-col>
-                </el-row>
-            </el-header>
-        <el-container>
-            <el-aside width="200px">
-                <!-- 侧边栏 -->
-                <!-- 常用属性： router:boolean  启用该模式会在激活导航时以 index 作为 path 进行路由跳转
-                  unique-opened:boolean   是否只保持一个子菜单的展开-->
-                <!-- default-active 通过index 属性,将符合条件的作为默认显示 -->
-            <el-menu
-            :router = 'true'
-            :unique-opened = 'true'
-            default-active="2"
-            class="el-menu-vertical-demo">
-                <el-submenu index="1">
-                    <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>用户管理</span>
-                    </template>
-                    <!-- 这里的index 相当于router-link  index的值相当于路由的path -->
-                    <el-menu-item index="users">
-                    <i class="el-icon-menu"></i>
-                        用户列表</el-menu-item>
-                </el-submenu>
+  <el-container>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <img src="@/assets/logo.png">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a class="layout" @click.prevent="homeExit()" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!-- 侧边栏 -->
+        <!-- 常用属性： router:boolean  启用该模式会在激活导航时以 index 作为 path 进行路由跳转
+        unique-opened:boolean   是否只保持一个子菜单的展开-->
+        <!-- default-active 通过index 属性,将符合条件的作为默认显示 -->
+        <el-menu
+          :router="true"
+          :unique-opened="true"
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <!-- 这里的index 相当于router-link  index的值相当于路由的path -->
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
 
-                <el-submenu index="2">
-                    <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>权限管理</span>
-                    </template>
-                    <el-menu-item index="1-2">
-                    <i class="el-icon-menu"></i>
-                        角色列表</el-menu-item>
-                    <el-menu-item index="1-3">
-                    <i class="el-icon-menu"></i>
-                        权限列表</el-menu-item>
-                </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
 
-                  <el-submenu index="3">
-                    <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>商品管理</span>
-                    </template>
-                    <el-menu-item index="1-2">
-                    <i class="el-icon-menu"></i>
-                        商品列表</el-menu-item>
-                    <el-menu-item index="1-3">
-                    <i class="el-icon-menu"></i>
-                        分类参数</el-menu-item>
-                     <el-menu-item index="1-3">
-                     <i class="el-icon-menu"></i>
-                    商品分类</el-menu-item>
-                </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="1-3">
+              <i class="el-icon-menu"></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
 
-                 <el-submenu index="4">
-                    <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>订单管理</span>
-                    </template>
-                    <el-menu-item index="1-2">
-                    <i class="el-icon-menu"></i>
-                        订单列表</el-menu-item>
-                  </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
 
-                <el-submenu index="5">
-                    <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span>数据统计</span>
-                    </template>
-                    <el-menu-item index="1-2">
-                    <i class="el-icon-menu"></i>
-                        数据报表</el-menu-item>
-                </el-submenu>
-            </el-menu>
-
-            </el-aside>
-            <el-main>
-                <!-- 这里展示router-view 要渲染的组件 -->
-                <router-view></router-view>
-            </el-main>
-        </el-container>
-        </el-container>
-    </div>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              数据报表
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <!-- 这里展示router-view 要渲染的组件 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -123,20 +129,19 @@ export default {
 </script>
 
 <style>
-    .header {
-        height: 100%;
-        background: #324152;
-        color: #fff;
-    }
-    .middle {
-        text-align: center;
-        line-height: 60px;
-        text-align: center;
-    }
-    .layout {
-        line-height: 60px;
-        text-decoration: none;
-        color:rgb(57, 161, 221);
-    }
-
+.header {
+  height: 100%;
+  background: #324152;
+  color: #fff;
+}
+.middle {
+  text-align: center;
+  line-height: 60px;
+  text-align: center;
+}
+.layout {
+  line-height: 60px;
+  text-decoration: none;
+  color: rgb(57, 161, 221);
+}
 </style>
--- src/components/users.vue
+++ src/components/users.vue
@@ -1,5 +1,4 @@
 <template>
-  <div id="main">
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <el-breadcrumb separator="/">
@@ -13,58 +12,82 @@
 
       <el-row>
         <el-col>
-          <el-input placeholder="请输入内容" v-model="query" class="input-with-select search">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input
+            @clear="renderList()"
+            clearable
+            placeholder="请输入内容"
+            v-model="query"
+            class="input-with-select search"
+          >
+            <el-button @click="searchUsers()" slot="append" icon="el-icon-search"></el-button>
           </el-input>
-          <el-button type="success" plain>添加用户</el-button>
+          <el-button @click="dialogFormVisible = true" type="success" plain>添加用户</el-button>
         </el-col>
       </el-row>
+
+      <!-- 添加用户弹出框 -->
+      <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+          <el-form-item label="用户名" :label-width="formLabelWidth">
+            <el-input v-model="form.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" :label-width="formLabelWidth">
+            <el-input v-model="form.password" autocomplete="off"></el-input>
+          </el-form-item>
+           <el-form-item label="邮箱" :label-width="formLabelWidth">
+            <el-input v-model="form.email" autocomplete="off"></el-input>
+          </el-form-item>
+           <el-form-item label="电话" :label-width="formLabelWidth">
+            <el-input v-model="form.mobile" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+      </el-dialog>
+
       <!-- 表格部分 -->
       <el-table :data="tableData" style="width: 100%">
-          <!-- prop 依赖数据的键名 -->
+        <!-- prop 依赖数据的键名 -->
         <el-table-column prop="id" label="#" width="60"></el-table-column>
         <el-table-column prop="username" label="姓名" width="120"></el-table-column>
         <el-table-column prop="email" label="邮箱" width="140"></el-table-column>
         <el-table-column prop="mobile" label="电话" width="120"></el-table-column>
 
         <el-table-column prop="create_time" label="创建日期" width="120">
-            <template slot-scope="scope">
-                <!-- 自动依赖外层的数据 tableData -->
-                {{scope.row.create_time | fmtDate}}
-            </template>
+          <template slot-scope="scope">
+            <!-- 自动依赖外层的数据 tableData -->
+            {{scope.row.create_time | fmtDate}}
+          </template>
         </el-table-column>
 
         <el-table-column prop="mg_state" label="用户状态" width="80">
-            <template slot-scope="scope">
-                <el-switch
-                  v-model="scope.row.mg_state"
-                  active-color="#13ce66"
-                  inactive-color="#ff4949">
-                </el-switch>
-            </template>
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          </template>
         </el-table-column>
         <el-table-column prop="address" label="操作" width="140">
-            <template slot-scope="scope">
-                  <el-button type="primary" icon="el-icon-edit" circle size='mini' plain></el-button>
-                  <el-button type="danger" icon="el-icon-delete" circle size='mini' plain></el-button>
-                  <el-button type="success" icon="el-icon-check" circle size='mini' plain></el-button>
-            </template>
+          <!-- <template slot-scope="scope"> -->
+            <el-button type="primary" icon="el-icon-edit" circle size="mini" plain></el-button>
+            <el-button type="danger" icon="el-icon-delete" circle size="mini" plain></el-button>
+            <el-button type="success" icon="el-icon-check" circle size="mini" plain></el-button>
+          <!-- </template> -->
         </el-table-column>
       </el-table>
 
-        <!-- 分页部分 -->
-        <el-pagination
+      <!-- 分页部分 -->
+      <el-pagination
         class="paging"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="pagenum"
-      :page-sizes="[1, 2, 3, 4]"
-      :page-size="2"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[1, 2, 3, 4]"
+        :page-size="2"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
-  </div>
 </template>
 
 <script>
@@ -78,10 +101,23 @@ export default {
       //   每页显示数量
       pagesize: 2,
       // 值为-1,为了区分和后台拿到的数据,后台数据最少为0
-      total: -1
+      total: -1,
+      form: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      formLabelWidth: '120px',
+      dialogFormVisible: false
     }
   },
   methods: {
+    // 搜索用户
+    searchUsers () {
+      // 传入了query参数,根据query参数去获取用户信息
+      this.renderList()
+    },
     // 点击下拉框中的每页条数时触发
     handleSizeChange (val) {
       console.log(`每页 ${val} 条`)
@@ -100,9 +136,16 @@ export default {
       const AUTH_TOKEN = localStorage.getItem('token')
       this.$http.defaults.headers.common['Authorization'] = AUTH_TOKEN
       // 接口文档要求的查询字符串中要携带的参数
-      const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+      const res = await this.$http.get(
+        `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${
+          this.pagesize
+        }`
+      )
       // console.log(res)
-      const {data, meta: {status}} = res.data
+      const {
+        data,
+        meta: { status }
+      } = res.data
       if (status === 200) {
         this.total = data.total
         this.tableData = data.users
